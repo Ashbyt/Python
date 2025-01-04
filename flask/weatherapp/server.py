64c41a411d47ae9229d4ffc4863259fcdd4c001d
Ashley Bythell <abythell@Hotmail.com>
2025-01-04 12:01:22 +1100

    Update server.py

--- flask/weatherapp/server.py
+++ flask/weatherapp/server.py
@@ -1,4 +1,5 @@
 import requests
+#https://openweathermap.org/api
 api_key = ''
 location = input('please enter the location to look up weather in: ')
 response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={location}')
