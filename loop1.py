09a3b8d35a9ba1fd4af79ec999f2f20068a8d312
Ashley Bythell <abythell@Hotmail.com>
2025-06-04 21:34:45 +1000

    Create loop1.py

---
+++ loop1.py
@@ -0,0 +1,9 @@
+word = "chupacabra"
+enteredword = input("enter the secret word")
+while enteredword != word:
+if enteredword = word: 
+    break
+else
+enteredword = input("enter the secret word")
+
+print("You've successfully left the loop.")
